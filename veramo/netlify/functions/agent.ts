5165b4b36e2219138bff69ffdd35db08b8577d66
amritc2025 <achutoorgoon@gmail.com>
2025-07-28 16:27:56 +0400

    Add agent.ts to functions folder

---
+++ veramo/netlify/functions/agent.ts
@@ -0,0 +1,97 @@
+//agent.ts
+import {
+  createAgent,
+  IDIDManager,
+  IKeyManager,
+  IResolver,
+  ICredentialIssuer,
+} from '@veramo/core'
+
+import { CredentialPlugin } from '@veramo/credential-w3c'
+import { DIDManager } from '@veramo/did-manager'
+import { KeyManager } from '@veramo/key-manager'
+import { DIDResolverPlugin } from '@veramo/did-resolver'
+import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
+import { KeyDIDProvider } from '@veramo/did-provider-key'
+import { Resolver } from 'did-resolver'
+import { getDidKeyResolver } from '@veramo/did-provider-key'
+import { CredentialIssuerLD, VeramoEd25519Signature2018 } from '@veramo/credential-ld'
+import { contexts } from '@digitalbazaar/credentials-context'
+
+import {
+  Entities,
+  KeyStore,
+  DIDStore,
+  PrivateKeyStore,
+  migrations,
+  DataStore,
+  DataStoreORM
+} from '@veramo/data-store'
+
+
+import { DataSource } from 'typeorm'
+import * as fs from 'fs'
+
+const KMS_SECRET_KEY = '4a92dd58289d4f65b9c412346c351f4e'
+
+const dbFile = 'database.sqlite'
+if (!fs.existsSync(dbFile)) {
+  fs.writeFileSync(dbFile, '')
+}
+
+const dbConnection = new DataSource({
+  type: 'sqlite',
+  database: dbFile,
+  synchronize: false,
+  migrations,
+  migrationsRun: true,
+  entities: Entities,
+})
+
+let agent: any // declare here
+
+const setupAgent = async () => {
+  await dbConnection.initialize()
+  console.log('✅ Database initialized and migrations run if needed')
+
+  agent = createAgent<IDIDManager & IKeyManager & ICredentialIssuer & IResolver>({
+    plugins: [
+      new KeyManager({
+        store: new KeyStore(dbConnection),
+        kms: {
+          local: new KeyManagementSystem(
+            new PrivateKeyStore(dbConnection, new SecretBox(KMS_SECRET_KEY))
+          ),
+        },
+      }),
+      new DIDManager({
+        store: new DIDStore(dbConnection),
+        defaultProvider: 'did:key',
+        providers: {
+          'did:key': new KeyDIDProvider({
+            defaultKms: 'local',
+          }),
+        },
+      }),
+      new CredentialPlugin(),
+      new CredentialIssuerLD({
+        suites: [new VeramoEd25519Signature2018()],
+        contextMaps: [contexts.get('https://www.w3.org/2018/credentials/v1')],
+      }),
+      new DIDResolverPlugin({
+        resolver: new Resolver({
+          ...getDidKeyResolver(),
+        }),
+      }),
+      new DataStore(dbConnection),
+  new DataStoreORM(dbConnection),
+    ],
+  })
+
+  console.log('✅ agent.ts module loaded')
+  console.log(`📂 Using SQLite DB: ${dbFile}`)
+}
+
+await setupAgent()
+export { agent,dbConnection }
+
