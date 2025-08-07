0601269976f32fa4fe806bfd592187b44945be6c
amritc2025 <achutoorgoon@gmail.com>
2025-08-07 20:58:17 +0400

    updated all ts files and created new

--- veramo/netlify/functions/credential-offer.ts
+++ veramo/netlify/functions/credential-offer.ts
@@ -1,26 +1,26 @@
-// netlify/functions/credential-offer.ts
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async () => {
-  const responsePayload = {
-    credential_issuer: 'https://mdl-project.netlify.app',
-    credential_endpoint: 'https://mdl-project.netlify.app/.netlify/functions/credential-issuer',
-    credentials_supported: [
-      {
-        id: 'MobileDrivingLicence',
-        types: ['VerifiableCredential', 'MobileDrivingLicence'],
-        format: 'ldp_vc',
-        cryptographic_binding_methods_supported: ['did'],
-        cryptographic_suites_supported: ['Ed25519Signature2018'],
+  const credentialOffer = {
+    credential_offer: {
+      credential_issuer: 'https://mdl-project.netlify.app',
+      credential_endpoint: 'https://mdl-project.netlify.app/.netlify/functions/credential-issuer',
+      credentials_supported: [
+        {
+          id: 'MobileDrivingLicence',
+          types: ['VerifiableCredential', 'MobileDrivingLicence'],
+          format: 'ldp_vc',
+          cryptographic_binding_methods_supported: ['did'],
+          cryptographic_suites_supported: ['Ed25519Signature2018'],
+        },
+      ],
+      grants: {
+        'urn:ietf:params:oauth:grant-type:pre-authorized_code': {
+          'pre-authorized_code': 'test-code-123',
+          user_pin_required: false,
+        },
       },
-    ],
-    grants: {
-  'urn:ietf:params:oauth:grant-type:pre-authorized_code': {
-    'pre-authorized_code': 'test-code-123',
-    user_pin_required: false,
-  },
-}
-
+    },
   }
 
   return {
@@ -29,6 +29,7 @@ export const handler: Handler = async () => {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     },
-    body: JSON.stringify(responsePayload),
+    body: JSON.stringify(credentialOffer),
   }
 }
+
